092daf29edecf9a191b370ac83469dd91cdfaae9
Adenike <adeyinkaadenike992@gmail.com>
2024-08-26 16:20:38 +0100

    commited

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import '../Styled/Login.css'
+import React from 'react';
+import '../Styled/Login.css';
+import Logo from "/Logo.png";
+import { EnvelopeSimple, Eye, Lock } from '@phosphor-icons/react';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className='logo_part'>
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="login-container">
+        <h4>Login to your account</h4>
+        <div className="fieldcontainer">
+          <EnvelopeSimple size={20} color='#fff' />
+          <div className='divider'></div>
+          <input type="text" placeholder='Email Address' />
+        </div>
+        <div className="fieldcontainer">
+          <div className="password-field">
+            <Lock size={20} color='#fff' />
+            <div className='divider'></div>
+            <input type="password" placeholder='Enter password' />
+          </div>
+          <div className="lock">
+            <Eye size={20} color='#fff' />
+          </div>
+        </div>
+        <h5>Forget Password?</h5>
+      </div>
+      <div className="center">
+        <h4>Are you a new user? Sign up</h4>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
